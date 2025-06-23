8cdf9c3afaa073a1bc3f7b168fde0e06cb904d20
Ghesker-afk <spacexz12345@gmail.com>
2025-06-22 21:21:52 -0300

    Adding traits and generic function

--- movies/src/main.rs
+++ movies/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Genre {
     Action,
     Comedy,
@@ -9,11 +9,57 @@ pub enum Genre {
     Documentary,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Movie {
     pub title: String,
     pub genre: Genre,
     pub rating: f32,
+}
+
+pub trait UserPreference {
+    fn likes(&self, movie: &Movie) -> bool;
+}
+
+pub struct ActionFan;
+
+impl UserPreference for ActionFan {
+    fn likes(&self, movie: &Movie) -> bool {
+        matches!(movie.genre, Genre::Action)
+    }
+}
+
+pub struct ComedyLover;
+
+impl UserPreference for ComedyLover {
+    fn likes(&self, movie: &Movie) -> bool {
+        matches!(movie.genre, Genre::Comedy)
+    }
+}
+
+pub struct PickyCritic {
+    pub minimum_rating: f32,
+}
+
+impl UserPreference for PickyCritic {
+    fn likes(&self, movie: &Movie) -> bool {
+        let nota_do_filme = movie.rating;
+        let nota_minima = self.minimum_rating;
+
+        let atende_exigencia = nota_do_filme >= nota_minima;
+        atende_exigencia
+    }
+}
+
+pub fn recommend_movies<T: UserPreference>(user: &T, catalog: &[Movie]) -> Vec<Movie> {
+    let mut result = Vec::new();
+
+    for movie in catalog {
+        if user.likes(movie) {
+            result.push(movie.clone());
+        }
+    }
+
+    result
 }
 
 fn main() {
